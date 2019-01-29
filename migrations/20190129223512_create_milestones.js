c01b8a7ef26010d06df53252df78f22a06956dce
sumedha <sumedhanarayanan@gmail.com>
2019-01-29 23:04:06 +0000

    Adds migrations

--- migrations/20190129223512_create_milestones.js
+++ migrations/20190129223512_create_milestones.js
@@ -4,6 +4,8 @@ exports.up = function(knex, Promise) {
     table.increments('id');
     table.string('description').notNullable();
     table.date('date_acheieved').notNullable();
+    table.integer('famous_person_id').notNullable();
+    table.foreign('famouse_person_id').references('famous_people');
 
   });
 };
